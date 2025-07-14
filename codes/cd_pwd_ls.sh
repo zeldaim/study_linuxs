46404cc8de1d86495d0d8b1d75b8419e9f4226be
zeldaim <siuj@naver.com>
2025-07-14 18:13:37 +0900

    Create cd_pwd_ls.sh

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,13 @@
+[yhc@localhost Downloads]$ cd /home/yhc/Music
+[yhc@localhost Music]$ pwd
+/home/yhc/Music
+[yhc@localhost Music]$ ls
+[yhc@localhost Music]$ cd /home/yhc/Picture
+bash: cd: /home/yhc/Picture: No such file or directory
+[yhc@localhost Music]$ cd /home/yhc/Pictures
+[yhc@localhost Pictures]$ pwd
+/home/yhc/Pictures
+[yhc@localhost Pictures]$ ls
+'Screenshot from 2025-07-14 18-09-28.png'
+[yhc@localhost Pictures]$ 
+#오늘 pwd와 ls와 cd 를 했습니다.
