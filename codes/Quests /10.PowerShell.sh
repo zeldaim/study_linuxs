d3e148f6a14072587147eab2b4c987e114931990
zeldaim <siuj@naver.com>
2025-07-19 22:01:21 +0900

    Searching Command 명령어

---
+++ codes/Quests /10.PowerShell.sh
@@ -0,0 +1,34 @@
+
+PS C:\Users\siuj0> cd C:\
+PS C:\> cd C:\develope\
+PS C:\develope> dir
+
+
+    디렉터리: C:\develope
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----      2025-07-14   오후 7:34      589891952 Docker Desktop Installer.exe
+-a----      2025-07-14  오후 11:08    12851544064 Rocky-9.6-x86_64-dvd.iso
+-a----      2025-07-14   오후 8:27      420928760 VMware-workstation-full-17.6.3-24583834.exe
+
+
+PS C:\develope> pwd
+
+Path
+----
+C:\develope
+
+
+PS C:\develope> ls
+
+
+    디렉터리: C:\develope
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+-a----      2025-07-14   오후 7:34      589891952 Docker Desktop Installer.exe
+-a----      2025-07-14  오후 11:08    12851544064 Rocky-9.6-x86_64-dvd.iso
+-a----      2025-07-14   오후 8:27      420928760 VMware-workstation-full-17.6.3-24583834.exe
